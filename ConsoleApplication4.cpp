9d13a03dd41cdb91fe73b0c760e534a4cd93dac8
Владимир Басков <sunrise22813@gmail.com>
2022-09-27 22:39:23 +0300

    s

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 
 int main(){
-    /*Vector a(2, 3, 6), b(4, 4, 7);
+    Vector a(2, 3, 6), b(4, 4, 7);
     cout << a * b << "\n";
     Complex c(3, 4), d(8, -5);
-    cout << c + d << "\n";*/
+    cout << c + d << "\n";
     Matrix e(4, 4);
     cin >> e;
     cout << e.det();
